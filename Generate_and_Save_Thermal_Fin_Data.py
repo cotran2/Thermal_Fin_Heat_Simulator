e29dbe84e09f060b3aeafad296df739ac3d2f415
HwanGoh <Hwan.Goh@gmail.com>
2019-11-18 11:43:36 -0600

    small update

--- Generate_and_Save_Thermal_Fin_Data.py
+++ Generate_and_Save_Thermal_Fin_Data.py
@@ -115,15 +115,15 @@ if __name__ == "__main__":
     #   Run Options and File Names  #
     #################################     
     #=== Number of Data ===#
-    num_data = 50000
+    num_data = 200
 
     #=== Select True or Test Set ===#
-    generate_train_data = 1
-    generate_test_data = 0
+    generate_train_data = 0
+    generate_test_data = 1
     
     #===  Select Parameter Type ===#
-    generate_nine_parameters = 0
-    generate_varying = 1
+    generate_nine_parameters = 1
+    generate_varying = 0
     
     #=== Select Thermal Fin Dimension ===#
     generate_2D = 0
