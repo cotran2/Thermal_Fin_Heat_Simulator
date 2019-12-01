b1e3e9ae7498e26ef227b8a386f3d0a5ee8ec00d
Hwan Goh <Hwan.Goh@gmail.com>
2019-12-01 00:02:06 -0600

    small update

--- Generate_and_Save_Thermal_Fin_Data.py
+++ Generate_and_Save_Thermal_Fin_Data.py
@@ -13,6 +13,7 @@ sys.path.append('..')
 import numpy as np
 import pandas as pd
 import mpi4py
+pdb.set_trace()
 import dolfin as dl
 import matplotlib as plt
 from Thermal_Fin_Heat_Simulator.Utilities.gaussian_field import make_cov_chol
