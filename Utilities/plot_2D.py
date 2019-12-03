5a81ed4cca2bc1262c50d87820c95528872912c2
HwanGoh <Hwan.Goh@gmail.com>
2019-12-02 18:08:33 -0600

    small update

--- Utilities/plot_2D.py
+++ Utilities/plot_2D.py
@@ -7,17 +7,17 @@ Created on Sun Nov 17 11:47:02 2019
               - To enter dolfin's own plotting code, use dl.plot(some_dolfin_object) wheresome_dolfin_object is a 3D object and an error will be thrown up
 """
 import matplotlib.pyplot as plt
-import dolfin.cpp as cpp
 
 import pdb #Equivalent of keyboard in MATLAB, just add "pdb.set_trace()"
 
-def plot_2D(obj, title):   
+def plot_2D(obj, title, fig_size):   
     if hasattr(obj, "cpp_object"):
         obj = obj.cpp_object()
+    plt.figure(figsize = fig_size) 
     ax = plt.gca()
     ax.set_aspect('equal')
-    plt.title(title)
-    return my_mplot_function(ax, obj)
+    plt.title(title) 
+    return my_mplot_function(ax, obj), ax
     
 def my_mplot_function(ax, f, **kwargs):
     mesh = f.function_space().mesh()
